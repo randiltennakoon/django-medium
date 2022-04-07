15f7daee8b05f21811faf8a2203ec45b7df70976
Randil Tennakoon <randil2016al@gmail.com>
2022-04-07 20:03:56 +0530

    made the home app modularised

--- home/views.py
+++ home/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,7 @@ from django.http import HttpResponse
 def home(request):
     # return HttpResponse('<h1>Welcome to Django!</h1>')
     return render(request, 'home/index.html', {})
+
+
+def about(request):
+    return render(request, 'home/about.html', {'year':datetime.today().year})
